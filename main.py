2cf81fea81bbf9aee212b7eb256233716d30c2f7
Ha Nguyen Khanh <31272516+habom2310@users.noreply.github.com>
2019-04-02 10:13:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -40,22 +40,6 @@ for i,train_path in enumerate(train_paths):
 print(df_train.head())
 
 # PRE-PROCESSING
-def prewhiten(x):
-    if x.ndim == 4:
-        axis = (1, 2, 3)
-        size = x[0].size
-    elif x.ndim == 3:
-        axis = (0, 1, 2)
-        size = x.size
-    else:
-        raise ValueError('Dimension should be 3 or 4')
-
-    mean = np.mean(x, axis=axis, keepdims=True)
-    std = np.std(x, axis=axis, keepdims=True)
-    std_adj = np.maximum(std, 1.0/np.sqrt(size))
-    y = (x - mean) / std_adj
-    return y
-
 def l2_normalize(x, axis=-1, epsilon=1e-10):
     output = x / np.sqrt(np.maximum(np.sum(np.square(x), axis=axis, keepdims=True), epsilon))
     return output
